4c445ce2fee7485707910c41fbca5e0cfb40178d
Dave Brown <soon@willget.com>
2016-04-03 00:45:17 -0400

    Update inject.js

--- inject.js
+++ inject.js
@@ -5,15 +5,12 @@ chrome.extension.sendMessage({}, function(response) {
 		
 /* DO NOT EDIT ABOVE THIS LINE, DEFAULT TEMPLATE */
 
-// Basically use any bookmarklet you want here made javascript
+// Basically use any bookmarklet/javascript to be loaded as a new script to any webpage you surf to in Google Chrome Browser
 
-		window.addEventListener('click', function (x) {
-	if (x.detail === 3) {
-		window.scroll(0,0);
-	}
-});
+window.addEventListener('click',function(x){if(x.detail===3){window.scroll(0,0)}});
 
 /* DO NOT EDIT BELOW THIS LINE, DEFAULT TEMPLATE */
+
 	}
 	}, 10);
 });
