47e83faaa74b953c72ef2cf0f372ca184bcb17e7
Dave Brown <soon@willget.com>
2016-04-02 23:52:53 -0400

    Update inject.js

--- inject.js
+++ inject.js
@@ -2,11 +2,18 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
+		
+/* DO NOT EDIT ABOVE THIS LINE, DEFAULT TEMPLATE */
+
+// Basically use any bookmarklet you want here made javascript
+
 		window.addEventListener('click', function (x) {
 	if (x.detail === 3) {
 		window.scroll(0,0);
 	}
 });
+
+/* DO NOT EDIT BELOW THIS LINE, DEFAULT TEMPLATE */
 	}
 	}, 10);
 });
