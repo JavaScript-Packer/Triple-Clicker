df9b2e607abc5a05bec7d38fc274609e5ff863c1
Dave Brown <soon@willget.com>
2016-04-03 01:02:29 -0400

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,16 +1,8 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-		
+//chrome.extension.sendMessage({},function(){var readyStateCheckInterval=setInterval(function(){if("complete"===document.readyState){clearInterval(readyStateCheckInterval);
 /* DO NOT EDIT ABOVE THIS LINE, DEFAULT TEMPLATE */
-
 // Basically use any bookmarklet/javascript to be loaded as a new script to any webpage you surf to in Google Chrome Browser
 
 window.addEventListener('click',function(x){if(x.detail===3){window.scroll(0,0)}});
 
 /* DO NOT EDIT BELOW THIS LINE, DEFAULT TEMPLATE */
-
-	}
-	}, 10);
-});
+//}},10)});
